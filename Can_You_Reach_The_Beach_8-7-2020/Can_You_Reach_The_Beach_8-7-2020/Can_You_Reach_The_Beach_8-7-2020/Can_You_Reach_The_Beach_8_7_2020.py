a8bdc17f19e7341f7e88582f16be62c7c8931bfe
Nolan <nolan.brenhaug@live.com>
2020-08-09 12:30:53 -0500

    Added functions for calculating nematode days

--- Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8_7_2020.py
+++ Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8-7-2020/Can_You_Reach_The_Beach_8_7_2020.py
@@ -1,1 +1,22 @@
 
+import random
+
+
+
+
+def pair():
+    return random.randint(0,1)
+    
+
+def day(nematodes, days=1):
+    total = nematodes;
+    for d in range(days):
+        nematodes = total
+        for i in range((int)(nematodes/2)):
+            total = total + pair()
+
+    return total
+
+while True:
+    print(day(2,4))
+    input()
